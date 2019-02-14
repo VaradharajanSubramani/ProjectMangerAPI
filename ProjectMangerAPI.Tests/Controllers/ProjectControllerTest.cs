98107058967865ca3c1a1ca71b28cbffb7b0cbeb
varadharajan <varadharajan.subramani@cognizant.com>
2019-02-14 14:47:07 +0530

    test proj

---
+++ ProjectMangerAPI.Tests/Controllers/ProjectControllerTest.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProjectMangerAPI.Controllers;
+using ProjectManager.Entities;
+
+namespace ProjectMangerAPI.Tests.Controllers
+{
+    [TestClass]
+   public class ProjectControllerTest
+    {
+        ProjectController prj = new ProjectController();
+        [TestMethod]
+        public void GetAllProjects_test()
+        {
+            var projs = prj.GetAllProjects();
+            Assert.AreNotEqual(projs.Count(), 5);
+        }
+
+        [TestMethod]
+        public void Getprojectdetails_test()
+        {
+            var proj = prj.GetprojectDetails(1);
+            Assert.AreEqual(proj.ProjectName, "Project1");
+        }
+
+        [TestMethod]
+        public void DeleteUser()
+        {
+            int prjId = 1005;
+            prj.DeleteProject(prjId);
+            var prjs = prj.GetAllProjects();
+            Assert.AreNotEqual(prjs.Where(t => t.ProjectID == prjId).Count(), 1);
+        }
+
+        [TestMethod]
+        public void SearchUsersByName()
+        {
+            var users = prj.SearchProjects("Project1");
+            Assert.AreEqual(users.Count, 1);
+        }
+
+        [TestMethod]
+        public void SortingUsers()
+        {
+            var prjs = prj.SortingProjects("StartDate");
+            Assert.AreNotEqual(prjs.Count, 6);
+        }
+    }
+}
---
+++ ProjectMangerAPI.Tests/Controllers/UsersControllerTest.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProjectMangerAPI.Controllers;
+using ProjectManager.Entities;
+
+namespace ProjectMangerAPI.Tests.Controllers
+{
+    [TestClass]
+    public class UsersControllerTest
+    {
+        UsersController usr = new UsersController();
+        [TestMethod]
+        public void GetAllUsers_test()
+        {
+            var users = usr.GetAllUsers();
+            Assert.AreEqual(users.Count(), 5);
+        }
+
+        [TestMethod]
+        public void GetUserdetails_test()
+        {
+            var user = usr.GetUserDetails(1);
+            Assert.AreEqual(user.LastName, "Subramani");
+        }
+
+        [TestMethod]
+        public void DeleteUser()
+        {
+            int userid = 1005;
+            usr.DeleteUser(userid);
+            var user = usr.GetAllUsers();
+            Assert.AreNotEqual(user.Where(t=>t.UserID ==userid).Count(),1);
+        }
+
+        [TestMethod]
+
+        public void SearchUsersByName()
+        {
+            var users = usr.SearchUsersByName("varadha");
+            Assert.AreEqual(users.Count,1);
+        }
+
+        [TestMethod]
+        public void SortingUsers()
+        {
+            var users = usr.SortingUsers("FirstName");
+            Assert.AreNotEqual(users.Count, 6);
+        }
+
+    }
+}
